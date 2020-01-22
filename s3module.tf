b4738eb6213de04af8757971d898a9e901f08df8
Anara KD <rodnaya2303@gmail.com>
2020-01-22 16:56:46 -0600

    corrects added

--- s3module.tf
+++ s3module.tf
@@ -1,29 +1,29 @@
-module "s3anara" { 
-  source = "./s3.module"
+module "s3anara1" { 
+  source = "./s3module"
 
   region = "us-east-1"
   bucket = "anaras_bucket"
 
 }
 
-module "s3anara" { 
-  source = "./s3.module"
+module "s3anara2" { 
+  source = "./s3module"
 
   region = "us-east-2"
   bucket = "anaras_bucket1"
 
 }
 
-module "s3anara" { 
-  source = "./s3.module"
+module "s3anara3" { 
+  source = "./s3module"
 
   region = "us-west-1"
   bucket = "anaras_bucket2"
 
 }
 
-module "s3anara" { 
-  source = "./s3.module"
+module "s3anara4" { 
+  source = "./s3module"
 
   region = "us-west-2"
   bucket = "anaras_bucket3"
